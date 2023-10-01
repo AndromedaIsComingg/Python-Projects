0ffc968708ce6ea270f74d5c86fd2d82e93fc621
andromedaiscomingg <prodbysteev@gmail.com>
2023-10-02 00:29:05 +0100

    Creating comments

--- Vowel_Eater/Vowel_eater.py
+++ Vowel_Eater/Vowel_eater.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
 
-#f = ["a", "e", "i", "o", "u"]
-#g = []
-#print ("Welcome, this app eats vowels _00_")
-#while True:
-#	word = input ("please enter a word:")
-#	word = word.casefold()
-#	if all (char.isalpha() for char in word):
-#		pass
-#	else:
-#		print ("not a valid word")
-#		continue
-#	for re in word:
-#		if not re in f:
-#			g.append(re)
-#	print (g)
-#	break 
-
-
-
+# Importing Regular expression
 import re
+
+# This line prints a welcome display
 print ("Welcome, this app eats vowels _00_")
+
+# Creating a loop flow
 while True:
+# This takes the user input
 	w = input ("please enter a word:")
+
+# This converts uppercases to lowercase
 	w = w.casefold()
+	
+# This regular expression substitutes a,e,i,o,u with an empty string
 	x = re.sub("[aeiou]", "", w)
+
+# This ensures that every character in the input is an alphabet
 	if not all (char.isalpha() for char in w):
 		print (w, "is not a valid word")
 		continue
