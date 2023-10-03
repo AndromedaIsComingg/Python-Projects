51593a19049de3dabd5af74d46afaacc2a35d67f
andromedaiscomingg <prodbysteev@gmail.com>
2023-10-03 01:00:19 +0100

    commitng new comments

--- Highest-Number-Finder/Highest-Number-Finder.py
+++ Highest-Number-Finder/Highest-Number-Finder.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
+# This line displays the welcome messageg
+print ('This app takes multiple numbers as input and tells you the highest one. _00_')
 
-print ('This app takes multiple numbers and tells you the highest one. _00_')
+# Creates a list of all inputed numbers
 x = []
+
+# Creates a list of a sorted version of the above list after slicing
 z = [0]
 print ()
+
+# Creates a loop flow
 while True:
-	num= input ('enter a number or press "s" to stop: ')
-	if num == "s" or num == "S":
-		print (f'the highest number is {z}')
+	num= input ('enter a number or press "s" to stop: ')	#Takes the user input
+	if num == "s" or num == "S":							#Terminates the code
+		print (f'the highest number is {z}')				#Prints the highest number
 		break
-	if all(char.isnumeric() for char in num):
+	if all(char.isnumeric() for char in num):				#Ensures that all input are numeric
 		pass
 	else:
-		print ("only numbers must be entered ")
+		print ("only numbers must be entered ")				#Wrong input warning message
 		continue
-	x.append(num)
-	x.sort()
-	z = x[-1]
+	x.append(num)											#Apppends the input to the list x
+	x.sort()												#Sorts the list x	
+	z = x[-1]												#Slices the list x and picks the last value
