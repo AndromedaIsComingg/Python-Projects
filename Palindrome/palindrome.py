713d045310733c877c3b3d5ef061a6d2ada8bf28
andromedaiscomingg <prodbysteev@gmail.com>
2023-10-06 14:28:24 +0100

    adding comments

--- Palindrome/palindrome.py
+++ Palindrome/palindrome.py
@@ -1,9 +1,14 @@
 #Palindrome is a word a word, phrase, or sequence that reads the same backwards as forwards, e.g. madam nun. This program tells if a word is a palindrome
 
-print ("Palindrome is a word a word, phrase, or sequence that reads the same backwards as forwards, e.g. madam nun. \nThis program tells if a word is a palindrome")
 
+# welcome message
+print ("Palindrome is a word a word, phrase, or sequence that reads \n the same backwards as forwards, e.g. madam, nun. \nThis program tells if a word is a palindrome")
+
+
+# Taking user input
 word = input('please enter a word: ')
 
+# List slicing
 palin = word[::-1]
 
 if word == palin:
