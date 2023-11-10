73e6f3d78d46f4f02ee8b3708c6cb5f048c69c15
andromedaiscomingg <prodbysteev@gmail.com>
2023-11-10 07:24:33 +0100

    Added time module & upload capacity

--- Network_Speed_Checker/Network_Speed.py
+++ Network_Speed_Checker/Network_Speed.py
@@ -1,15 +1,20 @@
 import speedtest
 import time
 speed_test = speedtest.Speedtest(secure= True)
-print("Checking network download speed..")
+
+print("Checking network speed..")
 time.sleep(2)
-print("Checking network download speed....")
+print("Checking network speed....")
 time.sleep(2)
-print("Checking network download speed.......")
+print("Checking network speed please wait.......")
 
 def bytes_to_mb (bytes):
-	KB = 1024 # Convert from bits per second to megabits per second
+	KB = 1024 # Converts from bits per second to megabits per second
 	MB = KB * 1024
 	return int(bytes/MB)
 download_speed = bytes_to_mb(speed_test.download())
-print (f"your download speed is {download_speed} MB")
+upload_speed = bytes_to_mb(speed_test.upload())
+
+
+print (f"your download speed is {download_speed} Mbps",)
+print (f"your upload speed is {upload_speed} Mbps")
