9fea0f07bb13996a9570cd2ed6b5755eae990868
andromedaiscomingg <prodbysteev@gmail.com>
2024-01-19 16:10:57 +0100

    added rising fx sound

--- Network_Speed_Checker/Network_Speed.py
+++ Network_Speed_Checker/Network_Speed.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import Label, Button, Scale
+from tkinter import Label, Button
 import speedtest
 import threading
 import time
@@ -9,7 +9,7 @@ class NetworkSpeedTestApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Network Speed Checker")
-        self.root.geometry("400x300")
+        self.root.geometry("400x200")
 
         self.label_result = Label(self.root, text="", font=("Helvetica", 14))
         self.label_result.pack(pady=10)
@@ -18,19 +18,40 @@ class NetworkSpeedTestApp:
         self.button_start_test.pack(pady=10)
 
         pygame.mixer.init()
-        pygame.mixer.music.load("/Users/Adebisi/Documents/Python_Projects/Network_Speed_Checker/sounds/backgroud.wav")  # Replace with the path to your sound file
-        self.volume = 0.5  # Initial volume
+
+        # Background sound
+        pygame.mixer.music.load("/Users/Adebisi/Documents/Python_Projects/Network_Speed_Checker/sounds/backgroud.wav")  # Replace with the path to your background sound file
+        self.volume = 0.2  # Initial volume for background sound
         pygame.mixer.music.set_volume(self.volume)
-        
+
+        # Start test sound
+        self.start_test_sound = pygame.mixer.Sound("/Users/Adebisi/Documents/Python_Projects/Network_Speed_Checker/sounds/fx.wav")  # Replace with the path to your start test sound file
+        self.start_test_volume = 1  # Initial volume for start test sound
+        self.start_test_sound.set_volume(self.start_test_volume)
+
+        # Create a channel for the start test sound
+        self.start_test_channel = pygame.mixer.Channel(1)
+
         # Play the background sound immediately
-        pygame.mixer.music.play(-1)  # -1 plays the sound in a loop
+        threading.Thread(target=self.play_background_sound, daemon=True).start()
 
         # Bind the window closing event to the method
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     def run_speed_test(self):
+        # Play the start test sound when the button is clicked
+        threading.Thread(target=self.play_start_test_sound).start()
+
         self.label_result.config(text="Checking network speed..")
         threading.Thread(target=self.perform_speed_test).start()
+
+    def play_background_sound(self):
+        # Play the background sound continuously
+        pygame.mixer.music.play(-1)
+
+    def play_start_test_sound(self):
+        # Set the volume for the start test sound and play it on the designated channel
+        self.start_test_channel.play(self.start_test_sound)
 
     def perform_speed_test(self):
         try:
@@ -48,11 +69,15 @@ class NetworkSpeedTestApp:
             self.label_result.config(text=result_text)
 
         except speedtest.ConfigRetrievalError:
-            error_text = "Error retrieving speed test configuration. Please check your internet connection."
+            error_text = "Error retrieving speed test configuration. \nPlease check your internet connection."
             self.label_result.config(text=error_text)
         except Exception as e:
             error_text = f"An error occurred: {str(e)}"
             self.label_result.config(text=error_text)
+
+        finally:
+            # Stop the start test sound when the network speed is displayed
+            self.start_test_channel.stop()
 
     def on_closing(self):
         # Stop the background sound when the user closes the app
