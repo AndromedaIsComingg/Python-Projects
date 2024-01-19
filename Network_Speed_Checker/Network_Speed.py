7259294540e3b2b131bc342a3a37b8e1e340b44d
andromedaiscomingg <prodbysteev@gmail.com>
2024-01-19 13:46:24 +0100

    handling network error

--- Network_Speed_Checker/Network_Speed.py
+++ Network_Speed_Checker/Network_Speed.py
@@ -3,13 +3,12 @@ from tkinter import Label, Button
 import speedtest
 import threading
 import time
+
 class NetworkSpeedTestApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Network Speed Checker")
-       
         self.root.geometry("400x200")
-        #self.root.configure(bg="#1E1E1E")
 
         self.label_result = Label(self.root, text="", font=("Helvetica", 14))
         self.label_result.pack(pady=30)
@@ -22,25 +21,32 @@ class NetworkSpeedTestApp:
         threading.Thread(target=self.perform_speed_test).start()
 
     def perform_speed_test(self):
-        speed_test = speedtest.Speedtest(secure=True)
+        try:
+            speed_test = speedtest.Speedtest(secure=True)
 
-        # Simulate a delay to show the progress
-        for _ in range(5):
-            time.sleep(2)
-            self.label_result.config(text=self.label_result.cget("text") + ".")
+            # Simulate a delay to show the progress
+            for _ in range(5):
+                time.sleep(2)
+                self.label_result.config(text=self.label_result.cget("text") + ".")
 
-        download_speed = self.bytes_to_mb(speed_test.download())
-        upload_speed = self.bytes_to_mb(speed_test.upload())
+            download_speed = self.bytes_to_mb(speed_test.download())
+            upload_speed = self.bytes_to_mb(speed_test.upload())
 
-        result_text = f"Your download speed is {download_speed} Mbps\nYour upload speed is {upload_speed} Mbps"
-        self.label_result.config(text=result_text)
+            result_text = f"Your download speed is {download_speed} Mbps\nYour upload speed is {upload_speed} Mbps"
+            self.label_result.config(text=result_text)
+
+        except speedtest.ConfigRetrievalError:
+            error_text = "Error retrieving speed test configuration \nPlease check your internet connection."
+            self.label_result.config(text=error_text)
+        except Exception as e:
+            error_text = f"An error occurred: {str(e)}"
+            self.label_result.config(text=error_text)
 
     @staticmethod
     def bytes_to_mb(bytes):
         KB = 1024  # Converts from bits per second to megabits per second
         MB = KB * 1024
         return int(bytes / MB)
-
 
 if __name__ == "__main__":
     root = tk.Tk()
